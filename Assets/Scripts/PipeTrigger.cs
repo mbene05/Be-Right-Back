bb3420eb49fa3b7cdc4456664f90ac61579ba0a3
DevChh <115056028+DevChhX9@users.noreply.github.com>
2026-03-03 22:10:03 -0500

    WrenchUpdate

--- Assets/Scripts/PipeTrigger.cs
+++ Assets/Scripts/PipeTrigger.cs
@@ -2,12 +2,29 @@ using UnityEngine;
 
 public class PipeTrigger : MonoBehaviour
 {
-     public Camera mainCamera;
+    public Camera mainCamera;
     public float cameraZPosition = -10f;
-
     public GameObject pipePuzzleRoom;
+    public string requiredItemName = "Wrench";
+
+    private HotbarManager hotbar;
+
+    void Start()
+    {
+        hotbar = FindObjectOfType<HotbarManager>();
+    }
+
     void OnMouseDown()
     {
-        mainCamera.transform.position = new Vector3( pipePuzzleRoom.transform.position.x,  pipePuzzleRoom.transform.position.y, cameraZPosition);
+        if (MapManager.IsOpen) return;
+        if (PinCodeMiniGame.IsOpen) return;
+
+        if (hotbar != null && !hotbar.HasItem(requiredItemName))
+        {
+            Debug.Log("You need a " + requiredItemName + " to access this.");
+            return;
+        }
+
+        mainCamera.transform.position = new Vector3(pipePuzzleRoom.transform.position.x, pipePuzzleRoom.transform.position.y, cameraZPosition);
     }
-} 
+}
