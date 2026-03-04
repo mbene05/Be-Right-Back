fbfbcabb7907809e19b20635aa4c1343c6c4f268
DevChh <115056028+DevChhX9@users.noreply.github.com>
2026-03-03 23:09:28 -0500

    new

--- Assets/Scripts/PipeTrigger.cs
+++ Assets/Scripts/PipeTrigger.cs
@@ -2,38 +2,10 @@ using UnityEngine;
 
 public class PipeTrigger : MonoBehaviour
 {
-<<<<<<< Updated upstream
-=======
     bool isOpen = false;
->>>>>>> Stashed changes
     public Camera mainCamera;
     public float cameraZPosition = -10f;
     public GameObject pipePuzzleRoom;
-<<<<<<< Updated upstream
-    public string requiredItemName = "Wrench";
-
-    private HotbarManager hotbar;
-    private bool isOpen = false;
-
-    void Start()
-    {
-        hotbar = FindObjectOfType<HotbarManager>();
-    }
-
-    void OnMouseDown()
-    {
-        if (MapManager.IsOpen) return;
-        if (PinCodeMiniGame.IsOpen) return;
-        if (isOpen) return;
-
-        if (hotbar != null && !hotbar.HasItem(requiredItemName))
-        {
-            Debug.Log("You need a " + requiredItemName + " to access this.");
-            return;
-        }
-
-        mainCamera.transform.position = new Vector3(pipePuzzleRoom.transform.position.x, pipePuzzleRoom.transform.position.y, cameraZPosition);
-=======
     public GameObject returnRoom;
     void OnMouseDown()
     {
@@ -51,6 +23,5 @@ public class PipeTrigger : MonoBehaviour
             isOpen = false;
              mainCamera.transform.position = new Vector3(returnRoom.transform.position.x,  returnRoom.transform.position.y, cameraZPosition);
         }
->>>>>>> Stashed changes
     }
 }
