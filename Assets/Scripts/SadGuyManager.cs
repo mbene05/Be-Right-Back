b350b6525f004e493e209c3c0056f018b0a8ac32
DevChh <115056028+DevChhX9@users.noreply.github.com>
2026-03-13 16:00:40 -0400

    DRAGNDROPUPDATE

--- Assets/Scripts/SadGuyManager.cs
+++ Assets/Scripts/SadGuyManager.cs
@@ -48,33 +48,7 @@ public class SadGuyManager : MonoBehaviour, IUsableWithItem
 
         if (!dialogueManager.dialogueStarted && !dialogueManager.choicesContainer.gameObject.activeSelf)
         {
-
-            if (pickUpDrink == false)
-            {
-                dialogueManager.StartDialogue(myInkJSON3, sadGuyVoice);
-            }
-
-            else
-            {
-                if (rightDrink == true)
-                {
-                    HotbarManager hotbar = Hotbar.GetComponent<HotbarManager>();
-                    BartenderManager selectedBartender = Bartender.GetComponent<BartenderManager>();
-                    selectedBartender.pickUpDrink = false;
-                    hotbar.getridofslot1();
-                    dialogueManager.StartDialogue(myInkJSON2, sadGuyVoice);
-                }
-
-                else
-                {
-                     HotbarManager hotbar = Hotbar.GetComponent<HotbarManager>();
-                     hotbar.getridofslot1();
-                      BartenderManager selectedBartender = Bartender.GetComponent<BartenderManager>();
-                     selectedBartender.pickUpDrink = false;
-                     dialogueManager.StartDialogue(myInkJSON, sadGuyVoice);
-                }
-            }
-            
+            dialogueManager.StartDialogue(myInkJSON3, sadGuyVoice);
         }
         
     }
