25b0dc83842690b09959b8c9f8de1283c5d2b607
Alyssa <alys2004@my.yorku.ca>
2026-03-14 19:56:38 -0400

    Charlie highlight

---
+++ Assets/Scripts/HighlightManager.cs
@@ -0,0 +1,32 @@
+using UnityEditor;
+using UnityEngine;
+
+public class HighlightManager : MonoBehaviour
+{
+    public DialogueManager dialogueManager;
+
+    public GameObject highlighted;
+
+    void OnMouseOver()
+    {
+        if (dialogueManager.dialogueStarted || dialogueManager.choicesContainer.gameObject.activeSelf)
+        {
+            highlighted.SetActive(false);
+        }
+        else
+        {
+            highlighted.SetActive(true);
+        }
+
+    }
+
+    void OnMouseExit()
+    {
+        highlighted.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        highlighted.SetActive(false);
+    }
+}
