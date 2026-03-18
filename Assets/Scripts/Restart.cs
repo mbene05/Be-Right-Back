5ec1799acf5061e45686c2c0469fdbf17cd22867
OkieDokiii <mbenedik7@outlook.com>
2026-03-18 04:47:38 -0400

    the end

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+         if (Input.GetKeyDown(KeyCode.R))
+        {
+             SceneManager.LoadScene("MainTitle");
+        }
+    }
+}
