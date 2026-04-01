ee317a1c9e8d96241325cce828ec097cda6b770c
OkieDokiii <mbenedik7@outlook.com>
2026-04-01 00:14:27 -0400

    more again

--- Assets/Scripts/BackScript.cs
+++ Assets/Scripts/BackScript.cs
@@ -8,10 +8,12 @@ public class BackScript : MonoBehaviour
     public GameObject tilereturnRoom;
     public GameObject trigger;
     public GameObject Arrow;
+    public GameObject Arrow2;
 
     void OnMouseDown()
     {
         Arrow.SetActive(true);
+        Arrow2.SetActive(true);
         trigger.GetComponent<TilePuzzleTrigger>().isOpen = false;
         mainCamera.transform.position = new Vector3(tilereturnRoom.transform.position.x, tilereturnRoom.transform.position.y, cameraZPosition);
     }
